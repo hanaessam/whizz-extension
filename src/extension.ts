ed79631b8deeba93e71559c2d90fc650bf98c420
habibaahmed11 <habibaahmed3372@gmail.com>
2024-06-20 15:36:48 +0300

    fix feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { SidebarProvider } from './panels/SidebarProvider';
-import { sendSelectedCodeToServer } from './vscode-gateway/helper-functions';
-
-
+import * as vscode from "vscode";
+import { SidebarProvider } from "./panels/SidebarProvider";
+import { sendSelectedCodeToServer } from "./vscode-gateway/helper-functions";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const sidebarProvider = new SidebarProvider(context.extensionUri);
-	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider(
-			"whizz-sidebar",
-			sidebarProvider
-		)
-	);
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider("whizz-sidebar", sidebarProvider)
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "whizz" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "whizz" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('whizz.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from whizz!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("whizz.helloWorld", () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage("Hello World from whizz!");
+  });
 
-
-	
-
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
-
-
-
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
