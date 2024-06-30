fb89b7a82a4634f2e65d73542f180d9cc5ee4948
habibaahmed11 <habibaahmed3372@gmail.com>
2024-06-30 03:29:11 +0300

    authentication

--- src/vscode-gateway/fix-code.ts
+++ src/vscode-gateway/fix-code.ts
@@ -1,23 +1,20 @@
-import * as vscode from 'vscode';
-import axios from 'axios';
-import { baseUri } from '../constants';
-import { getSelectedCode } from './helper-functions';
-import { replaceSelectedCode } from './replace-selected-code';
+import * as vscode from "vscode";
+import axios from "axios";
+import { baseUri } from "../constants";
+import { getSelectedCode } from "./helper-functions";
+import { replaceSelectedCode } from "./replace-selected-code";
 
 export async function fixSelectedCode(code: string) {
-    
-        try {
-            const response = await axios.post(`${baseUri}/openai/prompt`, {
-                type : "fix" , 
-                codesnippet : code ,
-            });
-            const fixedcode = response.data.answer;
-            vscode.window.showInformationMessage(fixedcode);
-            replaceSelectedCode(fixedcode);
-            return fixedcode;
-        } catch (error: any) {
-            vscode.window.showErrorMessage('Failed to fix code ' + error.message);
-        }
-    
+  try {
+    const response = await axios.post(`${baseUri}/openai/prompt`, {
+      type: "fix",
+      codesnippet: code,
+    });
+    const fixedcode = response.data.code;
+    vscode.window.showInformationMessage(fixedcode);
+    replaceSelectedCode(fixedcode);
+    return fixedcode;
+  } catch (error: any) {
+    vscode.window.showErrorMessage("Failed to fix code " + error.message);
+  }
 }
-
